e59e9a31eef61c8bb98b6289b75376badf83d1a7
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-13 16:08:12 +0200

    fixed problem of multiple definitions
    It was a mix as all the functions where static but the last one and none was implemented in the src file

--- include/lie_algebra_utilities.hpp
+++ include/lie_algebra_utilities.hpp
@@ -15,55 +15,28 @@
 #include <Eigen/Dense>
 
 
-static Eigen::Matrix3d skew(const Eigen::Vector3d &t_v) {
-
-    Eigen::Matrix3d v_hat;
-    v_hat <<  0   ,  -t_v(2),   t_v(1),
-            t_v(2),     0   ,  -t_v(0),
-           -t_v(1),   t_v(0),     0   ;
-
-    return v_hat;
-}
-
-static Eigen::MatrixXd ad(const Eigen::VectorXd &t_strain){
-    //  Decompose the strain
-    const Eigen::Vector3d k = t_strain.block<3,1>(0,0);
-    const Eigen::Vector3d gamma = t_strain.block<3,1>(3,0);
-
-    Eigen::MatrixXd ad(6,6);
-    ad << skew(k)    , Eigen::Matrix3d::Zero(),
-          skew(gamma),          skew(k) ;
-
-    return ad;
-}
-
-static Eigen::MatrixXd Ad(const Eigen::Matrix3d &t_R,
-                   const Eigen::Vector3d &t_r){
-    //  Decompose the strain
-
-    Eigen::Matrix<double, 6, 6> Ad;
-    Ad <<       t_R    , Eigen::Matrix3d::Zero(),
-          skew(t_r)*t_R,          t_R ;
-
-    return Ad;
-}
-
-static Eigen::MatrixXd DeltaAd(const Eigen::Matrix3d &t_R,
-                   const Eigen::Vector3d &t_r){
-    //  Decompose the strain
-
-    Eigen::Matrix<double, 6, 6> Ad;
-    Ad <<       t_R    , Eigen::Matrix3d::Zero(),
-          skew(t_r)*t_R,          t_R ;
-
-    return Ad;
-}
+Eigen::Matrix3d skew(const Eigen::Vector3d &t_v);
 
 
-Eigen::Quaterniond rotateAlongAxis(const double &t_angle, const Eigen::Vector3d &t_axis)
-{
-    const auto axis_projection = sin(t_angle/2)*t_axis;
-    return Eigen::Quaterniond(cos(t_angle/2), axis_projection[0], axis_projection[1], axis_projection[2]);
-}
+
+Eigen::MatrixXd ad(const Eigen::VectorXd &t_strain);
+
+
+
+Eigen::MatrixXd Ad(const Eigen::Matrix3d &t_R,
+                   const Eigen::Vector3d &t_r);
+
+
+
+Eigen::MatrixXd DeltaAd(const Eigen::Matrix3d &t_R,
+                   const Eigen::Vector3d &t_r);
+
+
+
+Eigen::Quaterniond rotateAlongAxis(const double &t_angle, const Eigen::Vector3d &t_axis);
+
+
+
+
 
 #endif // LIE_ALGEBRA_UTILITIES_H
