070f731ecdd2fed0db6930f7d79aedc4ad9c5984
Andrea Gotelli <Andrea.Gotelli@ls2n.fr>
2022-07-26 19:17:27 +0200

    testing

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,5 +9,17 @@ int main(int argc, char *argv[])
     std::cout << ::Chebyshev::getDn(5) << std::endl;
     std::cout << "Forward :\n   ::Chebyshev::D_NN :\n" << ::Chebyshev::getD_NN(5, 1, ::Chebyshev::INTEGRATION_DIRECTION::FORWARD) << "\n   ::Chebyshev::D_IN :\n" << ::Chebyshev::getD_IN(5,1, ::Chebyshev::INTEGRATION_DIRECTION::FORWARD) << std::endl;
     std::cout << "Backward :\n   ::Chebyshev::D_NN :\n" << ::Chebyshev::getD_NN(5, 1, ::Chebyshev::INTEGRATION_DIRECTION::BACKWARD) << "\n   ::Chebyshev::D_IN :\n" << ::Chebyshev::getD_IN(5,1, ::Chebyshev::INTEGRATION_DIRECTION::BACKWARD) << std::endl;
+
+    std::cout << "\n\n\n";
+    std::cout << "Forward integration points : ";
+    for(const auto point : ::Chebyshev::defineIntegrationPoints(11, ::Chebyshev::INTEGRATION_DIRECTION::FORWARD))
+        std::cout << point << ", ";
+    std::cout << "\n\n\n";
+
+    std::cout << "Backward integration points : ";
+    for(const auto point : ::Chebyshev::defineIntegrationPoints(11, ::Chebyshev::INTEGRATION_DIRECTION::BACKWARD))
+        std::cout << point << ", ";
+    std::cout << "\n\n\n";
+
     return 0;
 }
