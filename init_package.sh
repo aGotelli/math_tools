b15e731f4c2ac6c567de1d86034dfaca8faf7566
Andrea Gotelli <gotelliandrea@gmail.com>
2022-09-30 16:32:57 +0200

    added init package

---
+++ init_package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+#Setting up environment
+
+# Require sudo privilegies
+if [ $UID == 0 ]; then
+    echo "Please run this script without sudo:"
+    echo "$0 $*"
+    exit 1
+fi
+
+make_install_build_of_type(){
+    echo
+    echo
+    echo
+    echo
+    echo "Cmake and install build of type " $1
+    echo
+    echo
+    echo
+    echo
+    mkdir -p $1 && cd $1
+    cmake ../.. -DCMAKE_BUILD_TYPE=$1
+    make
+    sudo make install
+}
+
+echo
+echo
+echo
+echo
+echo "setting up build system"
+echo
+echo
+echo
+echo
+mkdir -p build && cd build
+
+make_install_build_of_type Debug
+cd ..
+make_install_build_of_type Release
